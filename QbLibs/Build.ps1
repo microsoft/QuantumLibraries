5be3c1578e165bdc47c48b8a5ca2e779b3278054
Guang Hao Low <gulow@microsoft.com>
2017-10-20 18:01:47 -0700

    More updates

--- QbLibs/Build.ps1
+++ QbLibs/Build.ps1
@@ -45,51 +45,29 @@ $qflatSources = @(Find-QflatPrelude)
 
 $qflatSources += @(
     # Provide stubs for primitive operations.
-    "Stubs.qb",
+    "Stubs.qb"
 
-    "Math/Types.qb",
-    "Math/Constants.qb",
+    "Math/NativeStubs.qb"
 
-    "IterateThroughCartesianProduct.qb",
+    "Combinators/RestrictToSubregister.qb"
 
-    "Combinators/ApplyToRange.qb",
-    "Enumeration/Iter.qb",
+    "DataStructures/Pairs.qb"
+    
+    "Enumeration/Iter.qb"
+    "Enumeration/Trotter.qb"
 
-    # Diagnostics
-    "Asserts/AssertQubit.qb",
-    "Asserts/AssertOperationsEqualReferenced.qb",
-    "Asserts/AssertOperationsEqualInPlace.qb",
+    "Simulation/Types.qb"
+    "Simulation/SimulationTechniques.qb"
+    "Simulation/EvolutionSetPauli.qb"
+    #"Simulation/EvolutionSetFermionic.qb"
 
-    # Provide definitions of the identity and nop.
-    "Identity.qb"
+    "Simulation/ExampleH2.qb"
+    "Simulation/ExampleIsing.qb"
 
-    # Endianness.qb contains newtype declarations that are needed more broadly,
-    # so we include it first.
-    "Endianness.qb",
+    #"Simulation/Schedule.qb"
 
-    "DataStructures/Stack.qb"
+    #"Enumeration/Iter2.qb"
 
-    # Similarly with OracleTypes.qb, save for that it depends on OperationPow.qb.
-    "Combinators/OperationPow.qb",
-    "PhaseEstimation/Types.qb",
-    "Arithmetic.qb",
-    "Bind.qb",
-
-    "QFT.qb",
-    "PhaseEstimation/Quantum.qb",
-    "PhaseEstimation/Iterative.qb",
-    # "AmplitudeAmplification.qb"
-    "ShiftOp.qb",
-    "Combinators/With.qb",
-
-    "Paulis.qb",
-
-    # QECC
-    "Qecc/Types.qb",
-    "Qecc/Utils.qb",
-    "Qecc/BitFlipCode.qb",
-    "Qecc/5QubitCode.qb",
-    "Qecc/7QubitCode.qb"
 ) | ForEach-Object {
     Join-Path $libDirectory $_
 }
