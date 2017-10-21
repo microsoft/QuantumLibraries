10a47ed3dcfe6fc3edd3a54ad3bda80777c4c27f
Guang Hao Low <gulow@microsoft.com>
2017-10-21 13:50:22 -0700

    Experimenting with unit tests

--- QbLibs/Microsoft.Quantum.Canon/Tests/Drivers.cs
+++ QbLibs/Microsoft.Quantum.Canon/Tests/Drivers.cs
@@ -13,8 +13,23 @@ namespace Microsoft.Quantum.Canon.Tests
             return new QuantumSimulator();
         }
 
+
+        [TestMethod]
+        public void testDriver()
+        {
+            var sim = GetSimulator();
+            EmptyTest.Run(sim).Wait();
+        }
+        [TestMethod]
+        public void testDriver2()
+        {
+            var sim = GetSimulator();
+            test234.Run(sim).Wait();
+        }
+
+
+
         /*
-         * NONE OF THE TESTS ARE PASSING :(
         [TestMethod]
         public void TeleportationDriver()
         {
@@ -50,5 +65,42 @@ namespace Microsoft.Quantum.Canon.Tests
             GroverTest.Run(sim, 1024, 10, 11).Wait();
         }
         */
+
+    }
+
+    [TestClass]
+    public class TestDrivers
+    {
+        public static IOperationFactory GetSimulator()
+        {
+            return new QuantumSimulator();
+        }
+
+        [TestMethod]
+        public void testDriver3()
+        {
+            var sim = GetSimulator();
+            var msg = test234.Run(sim).Result;
+            msg = Result.Zero;
+            msg = Result.One;
+        }
+
+
+
+        //var toffoli = new ToffoliSimulator();
+        //var result = HelloWorld.Run(toffoli, 12).Result;
+        //System.Console.Out.WriteLine(result);
+
+        //var qsim = new QuantumSimulator();
+        //var msg = Result.One;
+        //test123.Run(qsim).Wait();
+        //var r = TeleportTest.Run(qsim, msg).Result;
+        //System.Console.WriteLine($"Sent: {msg}; Received: {r}");
+
+        //msg = Result.Zero;
+        //r = TeleportTest.Run(qsim, msg).Result;
+        //System.Console.WriteLine($"Sent: {msg}; Received: {r}");
+
+        //System.Console.ReadKey();
     }
 }
