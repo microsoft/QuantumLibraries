00fb23d360769ba98494b54b20ce81ef6129041b
Chris Granade <Christopher.Granade@microsoft.com>
2017-10-23 17:08:15 +0000

    Merged PR 289: Add more native math functions
    This PR adds several new math functions, such as `Log`, `ArcTanh` and `Ceiling`.

--- QbLibs/Microsoft.Quantum.Canon/Math/Native.cs
+++ QbLibs/Microsoft.Quantum.Canon/Math/Native.cs
@@ -9,97 +9,168 @@ namespace Microsoft.Quantum.Canon.Native
 {
     public class Sin : Microsoft.Quantum.Canon.Sin
     {
-        public Sin(IOperationFactory m) : base(m)
-        {
-            this.Dependencies = new Type[] { };
-        }
 
-        public override Type[] Dependencies
-        {
-            get;
-        }
+        public Sin(IOperationFactory m) : base(m) { }
 
-        public override Func<double, double> Body
-        {
-            get => (theta) =>
+        public override Func<double, double> Body =>
+            (theta) =>
                 System.Math.Sin(theta);
-        }
-
-        public static System.Threading.Tasks.Task<double> Run(IOperationFactory m, Double theta)
-        {
-            return m.Run<Sin, double, double>(theta);
-        }
+        
     }
 
     public class Cos : Microsoft.Quantum.Canon.Cos
     {
-        public Cos(IOperationFactory m) : base(m)
-        {
-            this.Dependencies = new Type[] { };
-        }
 
-        public override Type[] Dependencies
-        {
-            get;
-        }
+        public Cos(IOperationFactory m) : base(m) { }
 
-        public override Func<double, double> Body
-        {
-            get => (theta) =>
+        public override Func<double, double> Body =>
+            (theta) =>
                 System.Math.Cos(theta);
-        }
 
-        public static System.Threading.Tasks.Task<double> Run(IOperationFactory m, Double theta)
-        {
-            return m.Run<Cos, double, double>(theta);
-        }
+    }
+
+    public class Tan : Microsoft.Quantum.Canon.Tan
+    {
+
+        public Tan(IOperationFactory m) : base(m) { }
+
+        public override Func<double, double> Body =>
+            (theta) =>
+                System.Math.Tan(theta);
+
     }
 
     public class ArcSin : Microsoft.Quantum.Canon.ArcSin
     {
-        public ArcSin(IOperationFactory m) : base(m)
-        {
-            this.Dependencies = new Type[] { };
-        }
+        
+        public ArcSin(IOperationFactory m) : base(m) { }
 
-        public override Type[] Dependencies
-        {
-            get;
-        }
-
-        public override Func<double, double> Body
-        {
-            get => (theta) =>
+        public override Func<double, double> Body =>
+            (theta) =>
                 System.Math.Asin(theta);
-        }
 
-        public static System.Threading.Tasks.Task<double> Run(IOperationFactory m, Double theta)
-        {
-            return m.Run<ArcSin, double, double>(theta);
-        }
     }
 
     public class ArcCos : Microsoft.Quantum.Canon.ArcCos
     {
-        public ArcCos(IOperationFactory m) : base(m)
-        {
-            this.Dependencies = new Type[] { };
-        }
+        
+        public ArcCos(IOperationFactory m) : base(m) { }
 
-        public override Type[] Dependencies
-        {
-            get;
-        }
-
-        public override Func<double, double> Body
-        {
-            get => (theta) =>
+        public override Func<double, double> Body =>
+            (theta) =>
                 System.Math.Acos(theta);
-        }
 
-        public static System.Threading.Tasks.Task<double> Run(IOperationFactory m, Double theta)
-        {
-            return m.Run<ArcCos, double, double>(theta);
-        }
+    }
+
+    public class ArcTan : Microsoft.Quantum.Canon.ArcTan
+    {
+
+        public ArcTan(IOperationFactory m) : base(m) { }
+
+        public override Func<double, double> Body =>
+            (theta) =>
+                System.Math.Atan(theta);
+
+    }
+
+    public class ArcTan2 : Microsoft.Quantum.Canon.ArcTan2
+    {
+
+        public ArcTan2(IOperationFactory m) : base(m) { }
+
+        public override Func<(double, double), double> Body =>
+            (args) =>
+                System.Math.Atan2(args.Item1, args.Item2);
+
+    }
+
+    public class Log : Microsoft.Quantum.Canon.Log
+    {
+
+        public Log(IOperationFactory m) : base(m) { }
+
+        public override Func<double, double> Body =>
+            (theta) =>
+                System.Math.Log(theta);
+
+    }
+
+    public class Ceiling : Microsoft.Quantum.Canon.Ceiling
+    {
+
+        public Ceiling(IOperationFactory m) : base(m) { }
+
+        public override Func<double, long> Body =>
+            (theta) =>
+                // FIXME: add range checking!
+                (long) System.Math.Ceiling(theta);
+
+    }
+
+    public class Sqrt : Microsoft.Quantum.Canon.Sqrt
+    {
+
+        public Sqrt(IOperationFactory m) : base(m) { }
+
+        public override Func<double, double> Body =>
+            (theta) =>
+                System.Math.Sqrt(theta);
+
+    }
+
+    public class Cosh : Microsoft.Quantum.Canon.Cosh
+    {
+
+        public Cosh(IOperationFactory m) : base(m) { }
+
+        public override Func<double, double> Body =>
+            (theta) =>
+                System.Math.Cosh(theta);
+
+    }
+
+    public class Sinh : Microsoft.Quantum.Canon.Sinh
+    {
+
+        public Sinh(IOperationFactory m) : base(m) { }
+
+        public override Func<double, double> Body =>
+            (theta) =>
+                System.Math.Sinh(theta);
+
+    }
+
+    public class Tanh : Microsoft.Quantum.Canon.Tanh
+    {
+
+        public Tanh(IOperationFactory m) : base(m) { }
+
+        public override Func<double, double> Body =>
+            (theta) =>
+                System.Math.Tanh(theta);
+
+    }
+
+    public class Floor : Microsoft.Quantum.Canon.Floor
+    {
+
+        public Floor(IOperationFactory m) : base(m) { }
+
+        public override Func<double, long> Body =>
+            (theta) =>
+                // FIXME: add range checking!
+                (long) System.Math.Floor(theta);
+
+    }
+
+    public class ToDouble : Microsoft.Quantum.Canon.ToDouble
+    {
+
+        public ToDouble(IOperationFactory m) : base(m) { }
+
+        public override Func<long, double> Body =>
+            (arg) =>
+                (double) arg;
+
     }
 }
--- QbLibs/Microsoft.Quantum.Canon/Tests/NativeOperationTests.cs
+++ QbLibs/Microsoft.Quantum.Canon/Tests/NativeOperationTests.cs
@@ -10,9 +10,9 @@ namespace Microsoft.Quantum.Canon.Tests
 {
     public class NativeOperationTests : TestBase
     {
-        [Theory(DisplayName ="Native trig")]
+        [Theory(DisplayName ="Native math")]
         [RandomDoubleData(nSamples: 5, min: -1, max: 1)]
-        public void TestNativeTrig(double theta)
+        public void TestNativeMath(double theta)
         {
             var sim = GetSimulator();
 
@@ -22,11 +22,38 @@ namespace Microsoft.Quantum.Canon.Tests
             var cos = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.Cos>();
             Assert.Equal(cos.Apply(theta), Math.Cos(theta));
 
+            var tan = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.Tan>();
+            Assert.Equal(tan.Apply(theta), Math.Tan(theta));
+
             var asin = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.ArcSin>();
             Assert.Equal(asin.Apply(theta), Math.Asin(theta));
 
             var acos = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.ArcCos>();
             Assert.Equal(acos.Apply(theta), Math.Acos(theta));
+            
+            var atan = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.ArcTan>();
+            Assert.Equal(atan.Apply(theta), Math.Atan(theta));
+
+            var sqrt = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.Sqrt>();
+            Assert.Equal(sqrt.Apply(theta), Math.Sqrt(theta));
+
+            var log = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.Log>();
+            Assert.Equal(log.Apply(theta), Math.Log(theta));
+
+            var sinh = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.Sinh>();
+            Assert.Equal(sinh.Apply(theta), Math.Sinh(theta));
+
+            var cosh = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.Cosh>();
+            Assert.Equal(cosh.Apply(theta), Math.Cosh(theta));
+
+            var tanh = sim.Get<ICallable<double, double>, Microsoft.Quantum.Canon.Tanh>();
+            Assert.Equal(tanh.Apply(theta), Math.Tanh(theta));
+
+            var ceil = sim.Get<ICallable<double, long>, Microsoft.Quantum.Canon.Ceiling>();
+            Assert.Equal(ceil.Apply(theta), Math.Ceiling(theta));
+
+            var floor = sim.Get<ICallable<double, long>, Microsoft.Quantum.Canon.Floor>();
+            Assert.Equal(floor.Apply(theta), Math.Floor(theta));
         }
     }
 }
