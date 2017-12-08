9b60bc5c7238e8bdb08a73d34efb0a3ba2523e28
Alan Geller <ageller@microsoft.com>
2017-12-08 23:08:47 +0000

    Merged PR 546: Fixed the spelling of received
    Fixed the spelling of received

--- Samples/Teleportation/Program.cs
+++ Samples/Teleportation/Program.cs
@@ -14,9 +14,9 @@ namespace Microsoft.Quantum.Examples.Teleportation {
 
             foreach (var idxRun in Enumerable.Range(0, 8)) {
                 var sent = rand.Next(2) == 0;
-                var recieved = TeleportClassicalMessage.Run(sim, sent).Result;
-                System.Console.WriteLine($"Round {idxRun}:\tSent {sent},\tgot {recieved}.");
-                System.Console.WriteLine(sent == recieved ? "Teleportation successful!!\n" : "\n");
+                var received = TeleportClassicalMessage.Run(sim, sent).Result;
+                System.Console.WriteLine($"Round {idxRun}:\tSent {sent},\tgot {received}.");
+                System.Console.WriteLine(sent == received ? "Teleportation successful!!\n" : "\n");
             }
 
             System.Console.WriteLine("\n\nPress Enter to exit...\n\n");
