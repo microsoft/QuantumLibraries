d39ea5cbcd8be36cb65c753903d06bf84e2fe0ab
Martin <martinro@microsoft.com>
2018-10-28 15:02:01 -0700

    cleaned up whitespace

--- Chemistry/src/DataModel/DataFormats/LiQuiD.cs
+++ Chemistry/src/DataModel/DataFormats/LiQuiD.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-
-
 using Microsoft.Quantum.Simulation.Core;
 
 using System.Text.RegularExpressions;
--- Chemistry/tests/ChemistryTests/TestSuiteRunner.cs
+++ Chemistry/tests/ChemistryTests/TestSuiteRunner.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-
 using Microsoft.Quantum.Simulation.XUnit;
 using Microsoft.Quantum.Simulation.Simulators;
 using Xunit.Abstractions;
