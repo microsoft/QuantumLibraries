f97df9aca7e1e6159d356e4d632d95020601b25a
Andres Paz <anpaz@microsoft.com>
2019-03-29 11:06:26 -0700

    IQ# auto-restart for Python and debug messages. (#50)
    * Improving error handling when kernel start/goes down
    
    * Rolling back version numbers until we can get it programmatically from IQ#

--- Interoperability/python/src/qsharp/__init__.py
+++ Interoperability/python/src/qsharp/__init__.py
@@ -29,7 +29,7 @@ except:
 ## EXPORTS ##
 
 __all__ = [
-    'compile', 'reload'
+    'compile', 'reload',
     'get_available_operations', 'get_available_operations_by_namespace',
     'get_workspace_operations',
     'packages',
@@ -54,6 +54,7 @@ def compile(code : str) -> Union[QSharpCallable, List[QSharpCallable]]:
     ]
     if len(ops) == 1:
         return ops[0]
+
 
 def reload() -> None:
     """
