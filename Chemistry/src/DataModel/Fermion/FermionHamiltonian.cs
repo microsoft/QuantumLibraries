c9681706c88f96ef95c78feacaf66b103584cf39
Guang Hao Low <guanghaolow@gmail.com>
2019-04-07 19:45:07 -0700

    Fix infinite loop due to implicit operator casting

--- Chemistry/src/DataModel/Fermion/FermionHamiltonian.cs
+++ Chemistry/src/DataModel/Fermion/FermionHamiltonian.cs
@@ -10,17 +10,6 @@ namespace Microsoft.Quantum.Chemistry.Fermion
         public FermionHamiltonian() : base() { }
 
         /// <summary>
-        /// Method for adding fermion term to a fermion Hamiltonian using double instead of 
-        /// DoubleCoeff type for the coefficients.
-        /// </summary>
-        /// <param name="term">Term to be added.</param>
-        /// <param name="coefficient">Coefficient of term.</param>
-        public void AddTerm(FermionTermHermitian term, double coefficient)
-        {
-            AddTerm(term, coefficient.ToDouble());
-        }
-
-        /// <summary>
         /// Method for collecting all distinct system (orbital) indices.
         /// </summary>
         /// <param name="orbitalIntegral">Collate orbital indices from this orbital integral.</param>
