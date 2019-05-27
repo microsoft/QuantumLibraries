0bcd57eb702fe7a484ff11f53a2b9a2fdf4c17d6
Andres Paz <anpaz@microsoft.com>
2019-05-27 11:06:13 -0700

    Missing PUBLIC_KEY for test assembly

--- Standard/src/Properties/AssemblyInfo.cs
+++ Standard/src/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Microsoft.Quantum.Standard.Tests")]
+[assembly: InternalsVisibleTo("Microsoft.Quantum.Standard.Tests" + SigningConstants.PUBLIC_KEY)]
