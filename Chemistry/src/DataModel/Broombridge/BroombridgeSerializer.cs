a56339c3261808b96cf66f83ec3559face2fdd10
Christopher Granade <cgranade@cgranade.com>
2020-05-13 14:23:59 -0700

    Add support for StreamReader to Broombridge data.

--- Chemistry/src/DataModel/Broombridge/BroombridgeSerializer.cs
+++ Chemistry/src/DataModel/Broombridge/BroombridgeSerializer.cs
@@ -41,31 +41,39 @@ namespace Microsoft.Quantum.Chemistry.Broombridge
 
         
         /// <summary>
-        /// Returns version number of Broombridge file.
+        ///     Returns version number of a Broombridge file.
         /// </summary>
-        /// <param name="filename">Broombridge file address.</param>
+        /// <param name="filename">Path to a Broombridge file.</param>
         /// <returns>Version number of Broombridge file</returns>
         public static VersionNumber GetVersionNumber(string filename)
         {
-            using (var reader = File.OpenText(filename))
+            using var reader = File.OpenText(filename);
+            return GetVersionNumber(reader);
+        }
+
+        /// <summary>
+        ///     Returns version number of a Broombridge file.
+        /// </summary>
+        /// <param name="reader">Stream for reading Broombridge data.</param>
+        /// <returns>Version number of Broombridge file</returns>
+        public static VersionNumber GetVersionNumber(TextReader reader)
+        {
+            var deserializer = new DeserializerBuilder().Build();
+            var data = deserializer.Deserialize<Dictionary<string, object>>(reader);
+            var schema = data["$schema"] as string;
+            VersionNumber versionNumber = VersionNumber.NotRecognized;
+            if(schema != null)
             {
-                var deserializer = new DeserializerBuilder().Build();
-                var data = deserializer.Deserialize<Dictionary<string, object>>(reader);
-                var schema = data["$schema"] as string;
-                VersionNumber versionNumber = VersionNumber.NotRecognized;
-                if(schema != null)
+                foreach (var kv in VersionNumberDict)
                 {
-                    foreach (var kv in VersionNumberDict)
+                    if (schema.Contains(kv.Key))
                     {
-                        if (schema.Contains(kv.Key))
-                        {
-                            versionNumber = kv.Value;
-                            break;
-                        }
+                        versionNumber = kv.Value;
+                        break;
                     }
                 }
-                return versionNumber;
             }
+            return versionNumber;
         }
 
 
@@ -73,26 +81,55 @@ namespace Microsoft.Quantum.Chemistry.Broombridge
         /// Returns Broombridge deserialized into the current version data structure.
         /// Data structure is automatically updated to the current Broombridge version.
         /// </summary>
-        /// <param name="filename">Broombridge file address.</param>
-        /// <returns>Deserializer Broombridge data strauture.</returns>
+        /// <param name="reader">Stream for reading Broombridge data.</param>
+        /// <returns>Deserializer Broombridge data structure.</returns>
+        public static Data DeserializeBroombridge(TextReader reader)
+        {
+            // We'll need the stream twice: once to get the version of
+            // Broombridge used to serialize the data,
+            // and again to actually deserialize once we know the right
+            // version. Since we can't actually duplicate the stream,
+            // we'll read into memory first, then use a string reader
+            // in both cases.
+            var rawData = reader.ReadToEnd();
+            var versionNumber = GetVersionNumber(new StringReader(rawData));
+            var stringReader = new StringReader(rawData);
+            return new Data(
+                versionNumber switch
+                {
+                    VersionNumber.v0_1 => DataStructures.Update(
+                        Deserialize<V0_1.Data>(stringReader)
+                    ),
+                    VersionNumber.v0_2 => Deserialize<V0_2.Data>(stringReader),
+                    _ => throw new System.InvalidOperationException(
+                        "Unrecognized Broombridge version number."
+                    )
+                }
+            );
+        }
+
+        /// <summary>
+        /// Returns Broombridge deserialized into the current version data structure.
+        /// Data structure is automatically updated to the current Broombridge version.
+        /// </summary>
+        /// <param name="filename">Path to a Broombridge file.</param>
+        /// <returns>Deserializer Broombridge data structure.</returns>
         public static Data DeserializeBroombridge(string filename)
         {
-            VersionNumber versionNumber = GetVersionNumber(filename);
-            var output = new V0_2.Data();
-            if (versionNumber == VersionNumber.v0_1)
-            {
-                output = DataStructures.Update(Deserialize<V0_1.Data>(filename));
-            }
-            else if (versionNumber == VersionNumber.v0_2)
-            {
-                output = Deserialize<V0_2.Data>(filename);
-            }
-            else
-            {
-                throw new System.InvalidOperationException("Unrecognized Broombridge version number.");
-            }
-            return new Data(output);
+            using var reader = File.OpenText(filename);
+            return DeserializeBroombridge(filename);
         }
+
+        /// <summary>
+        /// Generic deserializer from a file into a data structure of type `TData`.
+        /// </summary>
+        /// <typeparam name="TData">Type of data to be deserialized.</typeparam>
+        /// <param name="reader">Stream for reading Broombridge data.</param>
+        /// <returns></returns>
+        public static TData Deserialize<TData>(TextReader reader) =>
+            new DeserializerBuilder()
+                .Build()
+                .Deserialize<TData>(reader);
 
         /// <summary>
         /// Generic deserializer from a file into a data structure of type `TData`.
@@ -102,12 +139,8 @@ namespace Microsoft.Quantum.Chemistry.Broombridge
         /// <returns></returns>
         public static TData Deserialize<TData>(string filename)
         {
-            using (var reader = File.OpenText(filename))
-            {
-                return new DeserializerBuilder()
-                    .Build()
-                    .Deserialize<TData>(reader);
-            }
+            using var reader = File.OpenText(filename);
+            return Deserialize<TData>(reader);
         }
     }
 
