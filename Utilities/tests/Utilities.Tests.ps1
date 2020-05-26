9aa6dad5200e725eeb1c5af12ffa015e243efdec
Christopher Granade <cgranade@cgranade.com>
2020-05-26 13:26:41 -0700

    Add initial tests.

---
+++ Utilities/tests/Utilities.Tests.ps1
@@ -0,0 +1,17 @@
+Get-Module Microsoft.Quantum.Utilities | Remove-Module -Force
+Import-Module -Force (
+    Join-Path `
+        $PSScriptRoot `
+        "../src/PowerShell/Microsoft.Quantum.Utilities/Microsoft.Quantum.Utilities.psd1"
+)
+
+Describe 'GetQdkVersion' {
+    It 'Does not throw' {
+        { Get-QdkVersion } | Should Not Throw;
+    }
+
+    It 'Should have all expected versions' {
+        $version = Get-QdkVersion;
+        $version.Key.Count | Should Be 6;
+    }
+}
