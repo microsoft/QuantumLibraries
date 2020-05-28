65020dfd67b1f0f67556a4e483ba03a64ce43331
Christopher Granade <cgranade@cgranade.com>
2020-05-28 09:37:26 -0700

    Require Pester 4 or later.

--- Utilities/tests/Utilities.Tests.ps1
+++ Utilities/tests/Utilities.Tests.ps1
@@ -1,6 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+# Make sure we're running on Pester 4 or later.
+if ((Get-Module Pester | Select-Object -ExpandProperty Version | Select-Object -ExpandProperty Major) -lt 4) {
+    Write-Error "Pester version 4 or later required, but $(Get-Module Pester | Select-Object -ExpandProperty Version) found.";
+    return;
+}
+
 Get-Module Microsoft.Quantum.Utilities | Remove-Module -Force
 Import-Module -Force (
     Join-Path `
@@ -10,7 +16,7 @@ Import-Module -Force (
 
 Describe 'GetQdkVersion' {
     It 'Does not throw' {
-        { Get-QdkVersion } | Should -Not Throw;
+        { Get-QdkVersion } | Should -Not -Throw;
     }
 
     It 'Should have all expected versions' {
