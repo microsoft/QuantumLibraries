b1a6c32ebbb4ca61b319737e06a0306c250a00bc
Christopher Granade <cgranade@cgranade.com>
2020-06-04 12:53:50 -0700

    Removed unused metadata method.

--- Chemistry/src/Tools/Extensions.cs
+++ Chemistry/src/Tools/Extensions.cs
@@ -38,20 +38,6 @@ namespace Microsoft.Quantum.Chemistry.Tools
             command.Description = description;
             return command;
         }
-
-        internal static Broombridge.V0_2.Data WithDefaultMetadata(this Broombridge.V0_2.Data data)
-        {
-            // TODO: make a constant for well-known schema URLs.
-            data.Schema = "https://raw.githubusercontent.com/Microsoft/Quantum/master/Chemistry/Schema/broombridge-0.2.schema.json";
-            data.Format = Microsoft.Quantum.Chemistry.Broombridge.V0_2.Data.DefaultFormat;
-            data.Generator = new Broombridge.V0_1.Generator
-            {
-                Source = "qdk-chem",
-                Version = typeof(Convert).Assembly.GetName().Version.ToString()
-            };
-            data.Bibliography = new List<Broombridge.V0_1.BibliographyItem>();
-            return data;
-        }
     }
 }
 
