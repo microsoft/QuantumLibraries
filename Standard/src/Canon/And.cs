101b7dfaba102dfdcd0212e6e50c120b525849b6
Mathias Soeken <mathias.soeken@outlook.com>
2020-07-06 18:55:40 +0200

    Simulate ApplyAnd and ApplyLowDepthAnd classically (#292)

---
+++ Standard/src/Canon/And.cs
@@ -0,0 +1,82 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#nullable enable
+
+using System;
+using Microsoft.Quantum.Intrinsic;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Microsoft.Quantum.Canon
+{
+    public partial class ApplyAnd
+    {
+        /// <summary>
+        /// Uses CCNOT implementation when executed using ToffoliSimulator.
+        /// </summary>
+        public class Native : ApplyAnd
+        {
+            private ToffoliSimulator? simulator = null;
+            protected CCNOT? CCNOT { get; set; } = null;
+
+            public Native(IOperationFactory m) : base(m)
+            {
+                simulator = m as ToffoliSimulator;
+            }
+
+            public override void Init()
+            {
+                base.Init();
+
+                if (simulator != null)
+                {
+                    CCNOT = Factory.Get<CCNOT>(typeof(CCNOT));
+                }
+            }
+
+            public override Func<(Qubit, Qubit, Qubit), QVoid> Body => CCNOT?.Body ?? base.Body;
+
+            public override Func<(Qubit, Qubit, Qubit), QVoid> AdjointBody => CCNOT?.AdjointBody ?? base.AdjointBody;
+
+            public override Func<(IQArray<Qubit>, (Qubit, Qubit, Qubit)), QVoid> ControlledBody => CCNOT?.ControlledBody ?? base.ControlledBody;
+
+            public override Func<(IQArray<Qubit>, (Qubit, Qubit, Qubit)), QVoid> ControlledAdjointBody => CCNOT?.ControlledAdjointBody ?? base.ControlledAdjointBody;
+        }
+    }
+
+    public partial class ApplyLowDepthAnd
+    {
+        /// <summary>
+        /// Uses CCNOT implementation when executed using ToffoliSimulator.
+        /// </summary>
+        public class Native : ApplyLowDepthAnd
+        {
+            private ToffoliSimulator? simulator = null;
+            protected CCNOT? CCNOT { get; set; } = null;
+
+            public Native(IOperationFactory m) : base(m)
+            {
+                simulator = m as ToffoliSimulator;
+            }
+
+            public override void Init()
+            {
+                base.Init();
+
+                if (simulator != null)
+                {
+                    CCNOT = Factory.Get<CCNOT>(typeof(CCNOT));
+                }
+            }
+
+            public override Func<(Qubit, Qubit, Qubit), QVoid> Body => CCNOT?.Body ?? base.Body;
+
+            public override Func<(Qubit, Qubit, Qubit), QVoid> AdjointBody => CCNOT?.AdjointBody ?? base.AdjointBody;
+
+            public override Func<(IQArray<Qubit>, (Qubit, Qubit, Qubit)), QVoid> ControlledBody => CCNOT?.ControlledBody ?? base.ControlledBody;
+
+            public override Func<(IQArray<Qubit>, (Qubit, Qubit, Qubit)), QVoid> ControlledAdjointBody => CCNOT?.ControlledAdjointBody ?? base.ControlledAdjointBody;
+        }
+    }
+}
--- Standard/tests/ANDTests.qs
+++ Standard/tests/ANDTests.qs
@@ -45,6 +45,7 @@ namespace Microsoft.Quantum.Tests {
     }
 
     @Test("QuantumSimulator")
+    @Test("ToffoliSimulator")
     operation ApplyAndTest() : Unit {
         for (p1 in [false, true]) {
             for (p2 in [false, true]) {
@@ -56,6 +57,7 @@ namespace Microsoft.Quantum.Tests {
     }
 
     @Test("QuantumSimulator")
+    @Test("ToffoliSimulator")
     operation ControlledApplyAndTest() : Unit {
         for (numControls in 3..5) {
             for (assignment in 0..2^numControls - 1) {
