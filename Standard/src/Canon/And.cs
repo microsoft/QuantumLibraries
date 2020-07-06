866897a28faf115e4e20c35efb0c84c1435fdf59
Mathias Soeken <mathias.soeken@outlook.com>
2020-07-06 15:18:40 +0200

    Simulate ApplyAnd and ApplyLowDepthAnd classically

---
+++ Standard/src/Canon/And.cs
@@ -0,0 +1,80 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.Quantum.Intrinsic;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Microsoft.Quantum.Canon
+{
+    /// <summary>
+    /// Uses CCNOT implementation when executed using ToffoliSimulator.
+    /// </summary>
+    public partial class ApplyAnd
+    {
+        public class Native : ApplyAnd
+        {
+            private ToffoliSimulator simulator = null;
+            protected CCNOT CCNOT { get; set; } = null;
+
+            public Native(IOperationFactory m) : base(m)
+            {
+                simulator = m as ToffoliSimulator;
+            }
+
+            public override void Init()
+            {
+                base.Init();
+
+                if (simulator != null)
+                {
+                    CCNOT = Factory.Get<CCNOT>(typeof(CCNOT));
+                }
+            }
+
+            public override Func<(Qubit, Qubit, Qubit), QVoid> Body => CCNOT?.Body ?? base.Body;
+
+            public override Func<(Qubit, Qubit, Qubit), QVoid> AdjointBody => CCNOT?.AdjointBody ?? base.AdjointBody;
+
+            public override Func<(IQArray<Qubit>, (Qubit, Qubit, Qubit)), QVoid> ControlledBody => CCNOT?.ControlledBody ?? base.ControlledBody;
+
+            public override Func<(IQArray<Qubit>, (Qubit, Qubit, Qubit)), QVoid> ControlledAdjointBody => CCNOT?.ControlledAdjointBody ?? base.ControlledAdjointBody;
+        }
+    }
+
+    /// <summary>
+    /// Uses CCNOT implementation when executed using ToffoliSimulator.
+    /// </summary>
+    public partial class ApplyLowDepthAnd
+    {
+        public class Native : ApplyLowDepthAnd
+        {
+            private ToffoliSimulator simulator = null;
+            protected CCNOT CCNOT { get; set; } = null;
+
+            public Native(IOperationFactory m) : base(m)
+            {
+                simulator = m as ToffoliSimulator;
+            }
+
+            public override void Init()
+            {
+                base.Init();
+
+                if (simulator != null)
+                {
+                    CCNOT = Factory.Get<CCNOT>(typeof(CCNOT));
+                }
+            }
+
+            public override Func<(Qubit, Qubit, Qubit), QVoid> Body => CCNOT?.Body ?? base.Body;
+
+            public override Func<(Qubit, Qubit, Qubit), QVoid> AdjointBody => CCNOT?.AdjointBody ?? base.AdjointBody;
+
+            public override Func<(IQArray<Qubit>, (Qubit, Qubit, Qubit)), QVoid> ControlledBody => CCNOT?.ControlledBody ?? base.ControlledBody;
+
+            public override Func<(IQArray<Qubit>, (Qubit, Qubit, Qubit)), QVoid> ControlledAdjointBody => CCNOT?.ControlledAdjointBody ?? base.ControlledAdjointBody;
+        }
+    }
+}
