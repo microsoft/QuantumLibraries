7355d77c97135e3bfa4cebb0528c2e83fc4eca5e
Andres Paz <anpaz@microsoft.com>
2020-07-13 10:34:49 -0700

    rm version

--- Python/qsharp-chemistry/qsharp/version.py
+++
@@ -1,9 +0,0 @@
-# Auto-generated file, do not edit.
-##
-# version.py: Specifies the version of the qsharp package.
-##
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-##
-__version__ = "0.0.0.1"
-_user_agent_extra = ""
