7c5db88843893875c3b33cdae05407a4e6be5329
Raphael Koh <t-rkoh@microsoft.com>
2020-07-17 16:47:46 -0400

    Override ApplyToEach GetRuntimeMetadata

---
+++ Standard/src/Canon/Combinators/ApplyToEach.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Quantum.Simulation.Core;
+
+namespace Microsoft.Quantum.Canon
+{
+    public partial class ApplyToEach<__T__>
+    {
+        public override RuntimeMetadata GetRuntimeMetadata(IApplyData args) =>
+            new RuntimeMetadata() { IsComposite = true };
+    }
+
+    public partial class ApplyToEachC<__T__>
+    {
+        public override RuntimeMetadata GetRuntimeMetadata(IApplyData args) =>
+            new RuntimeMetadata() { IsComposite = true };
+    }
+
+    public partial class ApplyToEachA<__T__>
+    {
+        public override RuntimeMetadata GetRuntimeMetadata(IApplyData args) =>
+            new RuntimeMetadata() { IsComposite = true };
+    }
+
+    public partial class ApplyToEachCA<__T__>
+    {
+        public override RuntimeMetadata GetRuntimeMetadata(IApplyData args) =>
+            new RuntimeMetadata() { IsComposite = true };
+    }
+}
