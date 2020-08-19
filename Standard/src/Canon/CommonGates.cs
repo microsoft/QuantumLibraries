03a877d06e01a8e1f41c69c239c7f52a7616a67d
Raphael Koh <t-rkoh@microsoft.com>
2020-08-18 20:23:24 -0400

    Clean up code

--- Standard/src/Canon/CommonGates.cs
+++ Standard/src/Canon/CommonGates.cs
@@ -5,74 +5,55 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using Microsoft.Quantum.Simulation.Core;
 
 namespace Microsoft.Quantum.Canon
 {
     public partial class CX
     {
-        public override RuntimeMetadata? GetRuntimeMetadata(IApplyData args)
-        {
-            Debug.Assert(args.Value is ValueTuple<Qubit, Qubit>, $"Failed to retrieve runtime metadata for {this.ToString()}.");
-
-            if (args.Value is ValueTuple<Qubit, Qubit> cxArgs)
+        public override RuntimeMetadata? GetRuntimeMetadata(IApplyData args) =>
+            args.Value switch
             {
-                var (ctrl, target) = cxArgs;
-                return new RuntimeMetadata()
+                (Qubit ctrl, Qubit target) => new RuntimeMetadata()
                 {
                     Label = "X",
                     IsControlled = true,
                     Controls = new List<Qubit>() { ctrl },
                     Targets = new List<Qubit>() { target },
-                };
-            }
-
-            return null;
-        }
+                },
+                _ => throw new Exception($"Failed to retrieve runtime metadata for {this.ToString()}."),
+            };
     }
 
     public partial class CY
     {
-        public override RuntimeMetadata? GetRuntimeMetadata(IApplyData args)
-        {
-            Debug.Assert(args.Value is ValueTuple<Qubit, Qubit>, $"Failed to retrieve runtime metadata for {this.ToString()}.");
-
-            if (args.Value is ValueTuple<Qubit, Qubit> cyArgs)
+        public override RuntimeMetadata? GetRuntimeMetadata(IApplyData args) =>
+            args.Value switch
             {
-                var (ctrl, target) = cyArgs;
-                return new RuntimeMetadata()
+                (Qubit ctrl, Qubit target) => new RuntimeMetadata()
                 {
                     Label = "Y",
                     IsControlled = true,
                     Controls = new List<Qubit>() { ctrl },
                     Targets = new List<Qubit>() { target },
-                };
-            }
-
-            return null;
-        }
+                },
+                _ => throw new Exception($"Failed to retrieve runtime metadata for {this.ToString()}."),
+            };
     }
 
     public partial class CZ
     {
-        public override RuntimeMetadata? GetRuntimeMetadata(IApplyData args)
-        {
-            Debug.Assert(args.Value is ValueTuple<Qubit, Qubit>, $"Failed to retrieve runtime metadata for {this.ToString()}.");
-
-            if (args.Value is ValueTuple<Qubit, Qubit> czArgs)
+        public override RuntimeMetadata? GetRuntimeMetadata(IApplyData args) =>
+            args.Value switch
             {
-                var (ctrl, target) = czArgs;
-                return new RuntimeMetadata()
+                (Qubit ctrl, Qubit target) => new RuntimeMetadata()
                 {
                     Label = "Z",
                     IsControlled = true,
                     Controls = new List<Qubit>() { ctrl },
                     Targets = new List<Qubit>() { target },
-                };
-            }
-
-            return null;
-        }
+                },
+                _ => throw new Exception($"Failed to retrieve runtime metadata for {this.ToString()}."),
+            };
     }
 }
