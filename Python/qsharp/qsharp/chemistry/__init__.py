e71c8c51bbb4204d5e6c949eeaf2a3e51b5c8cd0
Guen Prawiroatmodjo <guenp@microsoft.com>
2020-11-05 15:42:18 -0800

    add try-except-else block to qsharp/chemistry/__init__.py

--- Python/qsharp/qsharp/chemistry/__init__.py
+++ Python/qsharp/qsharp/chemistry/__init__.py
@@ -4,7 +4,13 @@
 """This module imports the qsharp_chemistry package such that it can be imported via qsharp.chemistry.
 """
 
-from qsharp_chemistry.shims import load_input_state, load_fermion_hamiltonian, load_broombridge, encode
-from qsharp_chemistry.broombridge import Broombridge
-from qsharp_chemistry.fermion_hamiltonian import FermionHamiltonian
-from qsharp_chemistry.problem_description import ProblemDescription, InputState, IndexConvention
+try:
+    import qsharp_chemistry
+except ImportError:
+    raise ImportError("Missing dependency: please run `pip install qsharp-chemistry` to use the \
+        qsharp.chemistry subpackage.")
+else:
+    from qsharp_chemistry.shims import load_input_state, load_fermion_hamiltonian, load_broombridge, encode
+    from qsharp_chemistry.broombridge import Broombridge
+    from qsharp_chemistry.fermion_hamiltonian import FermionHamiltonian
+    from qsharp_chemistry.problem_description import ProblemDescription, InputState, IndexConvention
