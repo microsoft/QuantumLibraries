7387565496472d115f9073c46dda0333b70c2e4c
Guen Prawiroatmodjo <guenp@microsoft.com>
2020-11-09 14:31:26 -0800

    use find_packages instead of find_namespace_packages

--- Python/qsharp-chemistry/setup.py
+++ Python/qsharp-chemistry/setup.py
@@ -54,7 +54,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/microsoft/QuantumLibraries",
-    packages=setuptools.find_namespace_packages(include=["qsharp_chemistry.*"]),
+    packages=setuptools.find_packages(include=["qsharp_chemistry"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
