abae8a5b60fa6310282cfe305a3c6552b61fe5b6
Guen Prawiroatmodjo <guenp@microsoft.com>
2020-11-09 14:32:57 -0800

    move qsharp-core functionality to util.py, move startup script to init_qsharp and init_meta_path, explicitly set qsharp.client and other module-level globals

---
+++ Python/qsharp/qsharp/__init__.py
@@ -0,0 +1,40 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+import os
+from distutils.util import strtobool
+
+from qsharp.util import (
+    init_qsharp,
+    init_meta_path,
+    compile,
+    reload,
+    get_available_operations,
+    get_workspace_operations,
+    get_available_operations_by_namespace,
+    component_versions
+)
+
+from qsharp.clients.iqsharp import IQSharpError
+from qsharp.types import Result, Pauli
+
+# For debugging purposes only: set the environment variable 
+# INIT_QSHARP=False to skip autoloading the qsharp client
+if bool(strtobool(os.environ.get("INIT_QSHARP"))):
+    client, config, packages, projects = init_qsharp()
+    init_meta_path()
+
+
+__all__ = [
+    'compile', 
+    'reload',
+    'get_available_operations', 
+    'get_available_operations_by_namespace',
+    'get_workspace_operations',
+    'client',
+    'config',
+    'packages',
+    'projects',
+    'IQSharpError',
+    'Result', 
+    'Pauli'
+]
---
+++ Python/qsharp/qsharp/util.py
@@ -0,0 +1,142 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+"""This module contains general utilities for the Python/Q# interoperability feature.
+"""
+
+import sys
+
+from typing import List, Dict, Union, Tuple, TYPE_CHECKING
+from collections import defaultdict
+from distutils.version import LooseVersion
+
+from qsharp.clients import _start_client
+from qsharp.clients.iqsharp import IQSharpError
+from qsharp.loader import QSharpCallable, QSharpModuleFinder
+from qsharp.config import Config
+from qsharp.packages import Packages
+from qsharp.projects import Projects
+
+try:
+    from qsharp.version import __version__
+except:
+    __version__ = "<unknown>"
+
+if TYPE_CHECKING:
+    from qsharp.clients.iqsharp import IQSharpClient
+    from qsharp.clients.mock import MockClient
+
+__all__ = [
+    'compile', 
+    'reload',
+    'get_available_operations', 
+    'get_available_operations_by_namespace',
+    'get_workspace_operations',
+    'client',
+    'config',
+    'packages',
+    'projects'
+]
+
+def init_qsharp() -> Tuple[Union["IQSharpClient", "MockClient"], Config, Packages, Projects]:
+    """Initialize IQ# client and set up module finder.
+
+    :return: Tuple of client, config, packages and projects.
+    :rtype: tuple
+    """
+    global client, config, packages, projects
+
+    client = _start_client()
+    config = Config(client)
+    packages = Packages(client)
+    projects = Projects(client)
+
+    return client, config, packages, projects
+
+
+def init_meta_path():
+    """Add Q# module finder to meta path, declare qsharp as namespace to recognize subpackages
+    """
+    # Make sure that we're last on the meta_path so that actual modules are loaded
+    # first.
+    sys.meta_path.append(QSharpModuleFinder())
+
+    # Needed to recognize PEP 420 packages as subpackages.
+    import pkg_resources
+    pkg_resources.declare_namespace(__package__)
+
+
+def compile(code : str) -> Union[QSharpCallable, List[QSharpCallable]]:
+    """
+    Given a string containing Q# source code, compiles it into the current
+    workspace and returns one or more Q# callable objects that can be used to
+    invoke the new code.
+
+    :param code: A string containing Q# source code to be compiled.
+    :returns: A list of callables compiled from `code`, or a callable if exactly
+        one callable is found.
+    """
+    ops = [
+        QSharpCallable(op, "snippets")
+        for op in client.compile(code)
+    ]
+    if len(ops) == 1:
+        return ops[0]
+    else:
+        return ops
+
+
+def reload() -> None:
+    """
+    Reloads the current IQ# workspace, recompiling source files in the
+    workspace.
+
+    If the workspace fails to compile (e.g., because of a missing package),
+    Q# compilation errors are raised as an exception.
+    """
+    client.reload()
+
+
+def get_available_operations() -> List[str]:
+    """
+    Returns a list containing the names of all operations and functions defined
+    in the current workspace, and that have been compiled dynamically from
+    snippets.
+    """
+    return client.get_available_operations()
+
+
+def get_workspace_operations() -> List[str]:
+    """
+    Returns a list containing the names of all operations and functions defined
+    in the current workspace, excluding dynamically compiled snippets.
+    """
+    return client.get_workspace_operations()
+
+
+def get_available_operations_by_namespace() -> Dict[str, List[str]]:
+    """
+    Returns a dictionary from namespaces to lists of operations and functions
+    defined in the current workspace that are members of each namespace.
+    """
+    ops = get_available_operations()
+    by_ns = defaultdict(list)
+
+    for qualified_name in ops:
+        idx_last_dot = qualified_name.rfind(".")
+        ns_name = qualified_name[:idx_last_dot]
+        op_name = qualified_name[idx_last_dot + 1:]
+
+        by_ns[ns_name].append(op_name)
+
+    return dict(by_ns.items())
+
+
+def component_versions() -> Dict[str, LooseVersion]:
+    """
+    Returns a dictionary from components of the IQ# kernel to their
+    versions.
+    """
+    versions = client.component_versions()
+    # Add in the qsharp Python package itself.
+    versions["qsharp"] = LooseVersion(__version__)
+    return versions
